0dde1893d8b4cf4b0ad95ce3db2bbdb2534268d0
guruliciousjide@gmail.com <guruliciousjide@gmail.com>
2019-02-19 06:17:45 +0100

    Display the images in gridview

--- android/app/src/main/java/com/jideguru/wastatus/MainActivity.java
+++ android/app/src/main/java/com/jideguru/wastatus/MainActivity.java
@@ -1,17 +1,9 @@
 package com.jideguru.wastatus;
 
-import android.content.res.AssetManager;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import android.content.ContextWrapper;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
-import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 
@@ -49,12 +41,8 @@ public class MainActivity extends FlutterActivity {
         String path = Environment.getExternalStorageDirectory().toString()+"/Pictures/Twitter";
 
         List<String> imgs = new ArrayList<String>();
-//        String imgs;
-        Log.d("Files", "Path: " + path);
         File directory = new File(path);
         List<String> files = Arrays.asList(directory.list());
-        Log.d("Files", "files: "+ files.toString());
-
 
         imgs = files;
 
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,7 +1,11 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:async';
+import 'package:transparent_image/transparent_image.dart';
 import 'dart:io';
+
 
 class Home extends StatefulWidget {
   final String header;
@@ -12,15 +16,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //Channel Platform: Check MainActivity.java
   static const platform = const MethodChannel('samples.flutter.io/files');
   List imgs;
 
 
+
+  //Check the MainActivity.java
   Future<void> _getImages() async {
     List images;
     try {
       final List result = await platform.invokeMethod('getImages');
-      String l;
       images = result;
     } on PlatformException catch (e) {
       print("Error");
@@ -36,6 +42,47 @@ class _HomeState extends State<Home> {
   void initState() {
     _getImages();
   }
+
+
+  List<Widget> createImageCardItem(
+      List images, BuildContext context) {
+    // Children list for the list.
+    List<Widget> listElementWidgetList = new List<Widget>();
+    if (images != null) {
+      var lengthOfList = images.length;
+      for (int i = 0; i < lengthOfList; i++) {
+        String img_src = "/storage/emulated/0/Pictures/Twitter/"+images[i];
+        String img_name = images[i];
+        print(img_name);
+        // Image URL
+        // List item created with an image of the poster
+        var listItem = new GridTile(
+            footer: new GridTileBar(
+              backgroundColor: Colors.black45,
+              title: new Text(img_name),
+            ),
+            child: new GestureDetector(
+              onTap: () {
+                var router = new MaterialPageRoute(
+                    builder: (BuildContext context){
+//                      return Details(header: movie.title, img: imageURL, id: movie.id);
+                    }
+                );
+
+                Navigator.of(context).push(router);
+              },
+              child: new FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: img_src,
+                fit: BoxFit.cover,
+              ),
+            ));
+        listElementWidgetList.add(listItem);
+      }
+    }
+    return listElementWidgetList;
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -64,13 +111,20 @@ class _HomeState extends State<Home> {
 
 
 
-      body: Container(
-        child: Column(
-          children: <Widget>[
-
-            Text(imgs[1]),
-          ],
-        ),
+      body: CustomScrollView(
+        primary: false,
+        slivers: <Widget>[
+          new SliverPadding(
+            padding: const EdgeInsets.all(10.0),
+            sliver: new SliverGrid.count(
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children:
+              createImageCardItem(imgs, context),
+            ),
+          ),
+        ],
       ),
     );
   }
